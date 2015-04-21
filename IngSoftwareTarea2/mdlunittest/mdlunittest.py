c1b30425cdb8a2828d7a2505bc44b2e5cce72212
Javier Lopez <javierlopm@hotmail.com>
2015-04-21 18:13:18 -0430

    Nuevos casos frontera creados. Falla en verificacion de claves de la clase mdlaccescontrol

--- IngSoftwareTarea2/mdlunittest/mdlunittest.py
+++ IngSoftwareTarea2/mdlunittest/mdlunittest.py
@@ -16,6 +16,13 @@ class MdlTest(unittest.TestCase):
         self.acsc=mdlaccesscontrol.mdlaccesscontrol.clsAccessControl()
     
     """Casos Frontera"""
+    #Caso sin numeros
+    def testNoNum(self):
+        self.assertEqual("", self.acsc.encript("a@b!c-d$Oe"), "Error! la clave sin numeros se encripto!")
+    
+    #Caso sin letras
+    def testNoChars(self):
+        self.assertEqual("", self.acsc.encript("8#3-_-8$6!4"), "Error! la clave sin letras fue encriptada!")
     
     #Caso de string largo fuera del limite
     def testLargoNoEncript(self):
@@ -24,6 +31,22 @@ class MdlTest(unittest.TestCase):
     #Caso de string largo dentro del limite
     def testLargoEncript(self):
         self.assertNotEqual("", self.acsc.encript("12c45f78i!k2@456"), "Error! clave no encriptada")
+    
+    #Caso de string corto, fuera del limite inferior por un caracter
+    def testCortoNoEncript(self):
+        self.assertEqual("",self.acsc.encript("@2c!5f7"),"Error! se encripto cuando no debia!")
+        
+    #Caso de string corto, cerca del limite inferior por un caracter
+    def testCortoEncript(self):
+        self.assertNotEqual("",self.acsc.encript("@2c!5f7I"),"Error! clave no encriptada!")
+        
+    #Prueba de codificacion utf-8 clave con caracteres latinos
+    def testEnie(self):
+        self.assertNotEqual("",self.acsc.encript("ñ4oáóTí@úT"),"Error! clave no encriptada!")
+    
+    #Caso de string vacio
+    def testCadenaNula(self):
+        self.assertEqual("",self.acsc.encript(""), "Error! se obtuvo una encriptacion")
     
     
     
